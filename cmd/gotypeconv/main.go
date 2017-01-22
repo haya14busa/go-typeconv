5d66ae1ae2675cc69879e475d56942d98310f2e0
haya14busa <hayabusa1419@gmail.com>
2017-01-23 03:37:32 +0900

    cmd: support rewriting file

--- cmd/gotypeconv/main.go
+++ cmd/gotypeconv/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"go/format"
 	"go/printer"
 	"io"
 	"os"
@@ -12,25 +13,42 @@ import (
 	"golang.org/x/tools/go/loader"
 )
 
+type option struct {
+	write bool
+}
+
 func main() {
+	opt := &option{}
+	flag.BoolVar(&opt.write, "w", false, "write result to (source) file instead of stdout")
 	flag.Parse()
-	if err := run(os.Stderr, flag.Args()); err != nil {
+	if err := run(os.Stderr, flag.Args(), opt); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
 }
 
-func run(stderr io.Writer, args []string) error {
+func run(stderr io.Writer, args []string, opt *option) error {
 	prog, typeErrs, err := typeconv.Load(loader.Config{}, args)
 	if err != nil {
 		return err
 	}
 	for _, pkg := range prog.InitialPackages() {
 		for _, f := range pkg.Files {
+			filename := prog.Fset.File(f.Pos()).Name()
 			if err := typeconv.RewriteFile(prog.Fset, f, typeErrs); err != nil {
 				return err
 			}
-			if err := printer.Fprint(os.Stdout, prog.Fset, f); err != nil {
-				return err
+			if opt.write {
+				_ = format.Node
+				fh, err := os.Create(filename)
+				if err != nil {
+					return err
+				}
+				format.Node(fh, prog.Fset, f)
+				fh.Close()
+			} else {
+				if err := printer.Fprint(os.Stdout, prog.Fset, f); err != nil {
+					return err
+				}
 			}
 		}
 	}
