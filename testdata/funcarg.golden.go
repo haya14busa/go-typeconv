2d8709da2035b2b5835e157fcebe31cf9f0d3189
haya14busa <hayabusa1419@gmail.com>
2017-01-23 00:23:55 +0900

    testdata: add funcarg

---
+++ testdata/funcarg.golden.go
@@ -0,0 +1,9 @@
+package testdata
+
+func f() {
+	x := 1
+	funcarg(float64(x))
+}
+
+func funcarg(x float64) {
+}
---
+++ testdata/funcarg.input.go
@@ -0,0 +1,9 @@
+package testdata
+
+func f() {
+	x := 1
+	funcarg(x)
+}
+
+func funcarg(x float64) {
+}
